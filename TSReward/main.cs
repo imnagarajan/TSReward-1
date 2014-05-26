02860ee2e9d284fdeeba44bd1492f80eeedd16f3
Ancientgods <devoogdlaurens2@hotmail.com>
2014-05-26 20:01:56 +0200

    First public release

--- TSReward/main.cs
+++ TSReward/main.cs
@@ -4,20 +4,21 @@ using TShockAPI;
 using TerrariaApi.Server;
 using System.Reflection;
 using System.Net;
-using System.Collections.Generic;
 using System.IO;
 using Newtonsoft.Json;
 using Wolfje.Plugins.SEconomy;
+using Wolfje.Plugins.SEconomy.Journal;
+using Wolfje.Plugins.SEconomy.Economy;
+using System.Threading;
 
 namespace TSREWARD
 {
     [ApiVersion(1, 16)]
     public class TSReward : TerrariaPlugin
     {
-        public List<Message> VoteNotFoundMessages = new List<Message>();
-        public List<Message> OnRewardClaimMessages = new List<Message>();
         public static Config config;
         WebClient wc = new WebClient { Proxy = null };
+        public System.Timers.Timer Timer = new System.Timers.Timer();
         public override Version Version
         {
             get { return Assembly.GetExecutingAssembly().GetName().Version; }
@@ -40,6 +41,16 @@ namespace TSREWARD
         {
             Commands.ChatCommands.Add(new Command(Reward, "reward"));
             Commands.ChatCommands.Add(new Command("tsreward.reload", Reload_Config, "tsreload"));
+            ReadConfig();
+            Timer.Interval = 1000 * config.IntervalInSeconds;
+            Timer.Enabled = config.ShowIntervalMessage;
+            Timer.Elapsed += new System.Timers.ElapsedEventHandler(Timer_Elapsed);
+        }
+
+        void Timer_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            for (int i = 0; i < config.IntervalMessage.Text.Length; i++)
+                TSPlayer.All.SendMessage(config.IntervalMessage.Text[i], config.IntervalMessage.GetColor());
         }
 
         public TSReward(Main game)
@@ -58,33 +69,52 @@ namespace TSREWARD
 
         private void Reward(CommandArgs args)
         {
-            switch (CheckVote(args.Player.Name))
+            if (!args.Player.IsLoggedIn)
             {
-                case Response.Error:
-                    args.Player.SendErrorMessage("There was an issue reading your vote, this is either because");
-                    args.Player.SendErrorMessage("the server key is incorrect or terraria-servers.com is offline!");
-                    return;
-                case Response.NotFound:
-                    for (int i = 0; i < VoteNotFoundMessages.Count; i++)
-                        args.Player.SendMessage(VoteNotFoundMessages[i].Text, VoteNotFoundMessages[i].GetColor());
-                    return;
-                case Response.VotedAndClaimed:
-                    args.Player.SendInfoMessage("You have already claimed your reward today!");
-                    return;
-                case Response.VotedNotClaimed:
-                    for (int i = 0; i < OnRewardClaimMessages.Count; i++)
-                        args.Player.SendMessage(OnRewardClaimMessages[i].Text, OnRewardClaimMessages[i].GetColor());
-                    //hand out reward
-                    return;
+                args.Player.SendErrorMessage("You need to be logged in to use this command!");
+                return;
             }
+            Thread t = new Thread(() =>
+                {
+                    switch (CheckVote(args.Player.Name))
+                    {
+                        case Response.Error:
+                            args.Player.SendErrorMessage("There was an issue reading your vote, this is either because");
+                            args.Player.SendErrorMessage("the server key is incorrect or terraria-servers.com is offline!");
+                            return;
+                        case Response.NotFound:
+                            for (int i = 0; i < config.VoteNotFoundMessage.Text.Length; i++)
+                                args.Player.SendMessage(config.VoteNotFoundMessage.Text[i], config.VoteNotFoundMessage.GetColor());
+                            return;
+                        case Response.VotedAndClaimed:
+                            args.Player.SendErrorMessage("You have already claimed your reward today!");
+                            return;
+                        case Response.VotedNotClaimed:
+                            for (int i = 0; i < config.OnRewardClaimMessage.Text.Length; i++)
+                                args.Player.SendMessage(config.OnRewardClaimMessage.Text[i], config.OnRewardClaimMessage.GetColor());
+
+                            //if (config.UsingSeconomy)
+                            {
+                                EconomyPlayer Server = SEconomyPlugin.GetEconomyPlayerSafe(TSServerPlayer.Server.UserID);
+                                Server.BankAccount.TransferToAsync(args.Player.Index, config.SEconomyReward, config.AnnounceOnReceive ? BankAccountTransferOptions.AnnounceToReceiver : BankAccountTransferOptions.SuppressDefaultAnnounceMessages, "voting on terraria-servers.com", "Voted on terraria-servers.com");
+                                SetAsClaimed(args.Player.Name);
+                            }
+                            return;
+                    }
+                });
+            t.SetApartmentState(ApartmentState.STA);
+            t.Start();
+            
         }
 
         public Response CheckVote(string Username)
         {
-            int I = -1;
-            string s = wc.DownloadString(string.Format("http://terraria-servers.com/api/?object=votes&element=claim&key={0}&username={1}", config.ServerKey, Username));
-            int.TryParse(s, out I);
-            return (Response)I;
+            try
+            {
+                int I = int.Parse(wc.DownloadString(string.Format("http://terraria-servers.com/api/?object=votes&element=claim&key={0}&username={1}", config.ServerKey, Username)));
+                return (Response)I;
+            }
+            catch { return Response.Error; }
         }
 
         public void SetAsClaimed(string Username)
@@ -94,10 +124,10 @@ namespace TSREWARD
 
         public enum Response
         {
-            Error = -1,
             NotFound = 0,
             VotedNotClaimed = 1,
-            VotedAndClaimed = 2
+            VotedAndClaimed = 2,
+            Error=3
         }
 
         public class RewardItem
@@ -111,49 +141,57 @@ namespace TSREWARD
             }
         }
 
+        public class Clr
+        {
+            public int R;
+            public int G;
+            public int B;
+            public Clr(int r, int g, int b)
+            {
+                R = r;
+                G = g;
+                B = b;
+            }
+        }
+
         public class Message
         {
-            public string Text = "";
-            public string Color = "41,179,169";
-            public Message(string text)
+            public Clr Color;
+            public string[] Text;
+            public Message(string[] text)
             {
                 Text = text;
+                Color = new Clr(40,160,240);
             }
             public Color GetColor()
             {
-                try
-                {
-                    string[] split = Color.Split(',');
-                    float r = float.Parse(split[0]);
-                    float g = float.Parse(split[1]);
-                    float b = float.Parse(split[2]);
-                    return new Color(r, g, b);
-                }
-                catch
-                {
-                    Console.WriteLine("Error parsing color in config.json, proper color format: 000,000,000");
-                    Console.WriteLine("At line: " + Text);
-                    return new Color(41, 179, 169);
-                }
+                return new Color(Color.R, Color.G, Color.B);
             }
         }
 
         public class Config
         {
             public string ServerKey = "ServerKeyGoesHere";
-            public bool UsingSeconomy = false;
+            //public bool UsingSeconomy = false;
             public int SEconomyReward = 1000;
             public bool AnnounceOnReceive = true;
-            public RewardItem[] RewardItems = new RewardItem[] { new RewardItem(5, 1), new RewardItem(6, 1) };
-            public Message[] VoteNotFoundMessage = new Message[] {
-                new Message("Vote not found!"),
-                new Message("If you haven't voted yet, please go to terraria-servers.com and"),
-                new Message("vote for the server to receive ingame rewards!")
-            };
-            public Message[] OnRewardClaimMessage = new Message[] {
-                new Message("Thank you for voting on terraria-servers.com"),
-                new Message("We really appreciate it!")            
-            };
+            /*public bool GiveRewardItems = false;
+            public RewardItem[] RewardItems = new RewardItem[] { new RewardItem(5, 1), new RewardItem(6, 1) };*/
+            public Message VoteNotFoundMessage = new Message(new string[]{
+                "Vote not found!",
+                "If you haven't voted yet, please go to terraria-servers.com and",
+                "vote for the server to receive ingame rewards!"
+            });
+            public Message OnRewardClaimMessage = new Message(new string[] {
+                "Thank you for voting on terraria-servers.com",
+                "We really appreciate it!"          
+            });
+            public bool ShowIntervalMessage = true;
+            public int IntervalInSeconds = 300;
+            public Message IntervalMessage = new Message(new string[]{
+                "Vote on terraria-servers.com and receive 1000 coins!",
+                "After voting you can use the command /reward!"
+            });
         }
 
         private static void CreateConfig()
@@ -215,8 +253,8 @@ namespace TSREWARD
         {
             if (ReadConfig())
             {
-                VoteNotFoundMessages = new List<Message>(config.VoteNotFoundMessage);
-                OnRewardClaimMessages = new List<Message>(config.OnRewardClaimMessage);
+                Timer.Interval = 1000 * config.IntervalInSeconds;
+                Timer.Enabled = config.ShowIntervalMessage;
                 args.Player.SendMessage("TSReward config reloaded sucessfully.", Color.Green);
             }
         }
